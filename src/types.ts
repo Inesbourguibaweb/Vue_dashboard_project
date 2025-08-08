195fd75ed55a385d616419924d32873c5c6638ca
bourguibaines <ines@eggmed.com>
2025-08-08 12:18:34 +0100

    feat: Add types

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export interface Collaborateur {
+  id: number
+  nom: string
+  email: string
+  role: string
+}
+
+export interface Transaction {
+  id: number
+  date: string
+  montant: number
+  payee: boolean
+}
+
+export interface Summary {
+  total: number
+  totalPayee: number
+  totalNonPayee: number
+}
+
+export interface Client {
+  id: number
+  nom: string
+  email: string
+}
